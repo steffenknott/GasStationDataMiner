2fabea52c2f4e4c16b06a23711b4908b918c17d9
Steffen Knott <sk@edv-knott.de>
2018-04-24 22:43:32 +0000

    Finished initial version

--- pricesToInflux.py
+++ pricesToInflux.py
@@ -1,4 +1,4 @@
- Modul: pricesToInflux
+# Modul: pricesToInflux
 # Date: 24.04.2018
 # Author: steffenknott - Steffem Knott
 # Summary: Modul to fetch prices and push then into an InfluxDB instance.
@@ -9,22 +9,37 @@ import requests
 import json
 import doctest
 
+from influxdb import InfluxDBClient
+
 utils.loggingMode=3
+client = InfluxDBClient("10.0.0.21", 8086, "", "", "consumption")
 
 with open('tankstellen.conf','r') as stations:
   for station in stations:
     try:
-        utils.logMessage("Fetching station")
+        #utils.logMessage("Fetching station")
         jsonData=apiRequests.detailRequest(station.rstrip())
         if jsonData is None:
             utils.logMessage("Error while requesting details.")
         else:
-            utils.logMessage("Requesting details succeded.")
+            #utils.logMessage("Requesting details succeded.")
             with open('sorten.conf','r') as sorten:
+                json_body = [
+                {
+                    "measurement": "spritpreise",
+                    "tags": {
+                        "tankstelle": station.rstrip()
+                    },
+                    "fields": {
+                    }
+                }
+                ]
                 for sorte in sorten:
-                    dbquery= '{}={}'.format(sorte.rstrip(), jsonData["station"][sorte.rstrip()])
-                    print(dbquery)
-                    utils.logRequestResponse(jsonData)
+                    if sorte.rstrip() in jsonData["station"]:
+                        json_body[0]['fields'].update({sorte.rstrip(): jsonData["station"][sorte.rstrip()]})
+                print("INSERT JSON: {0}".format(json_body))
+                client.write_points(json_body)
+                 #       utils.logRequestResponse(jsonData)
     except Exception as exc:
             utils.logMessage("Exception while testing Requests: ", exc)
             raise SystemExit
